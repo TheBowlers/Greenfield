b9a46043ac457fdf9f71b3f7a5de4683e478d2ff
Kent Shepard <kentshepard7@gmail.com>
2017-09-29 19:40:39 -0400

    Create basic html and styling for MainPage/Login/App

---
+++ src/client/app/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+    )
+  }
+}
+
+export default Login;
