f928144af7b4ebd6d820cb7ef2bd9f3e8d75f2af
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-09-28 22:19:49 -0700

    Adds static page routing
    Installed express and Morgan and set npm script to start nodemon app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+//app.use(morgan({ format: "default" }));
+//app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+//app.use(morgan('tiny'));
+//app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
+app.use(morgan('[:date[clf]] | ":method :url" | STATUS: :status :res[content-length] ":referrer" '));
+
+
+app.set('port', 8080);
+
+
+const publicDir = '\\src\\client';
+app.use(express.static(__dirname + publicDir));
+console.log('public directory is: ' + __dirname + publicDir);
+
+
+if (!module.parent) {
+  app.listen(app.get('port'));
+  console.log('The Quizzard is listening on', app.get('port'));
+}
