be545df08c70d57758c0a915ed82555f07efef49
Zachary Biernat <zackbiernat@gmail.com>
2017-09-29 17:17:40 -0700

    Setting up questions.js and questions-handlers.js

--- src/server/db/models/questions.js
+++ src/server/db/models/questions.js
@@ -5,15 +5,15 @@ var app = require('../../../../app.js').app;
 var cors = require('cors');
 var bodyParser = require('body-parser');
 //Connection handler
-var MongoClient = require('./db/questions-handlers.js').MongoClient;
+var MongoClient = require('.././questions-handlers.js').MongoClient;
 //Request handlers
-var insertQ = require('./db/questions-handlers.js').insertQ;
-var findQ = require('./db/questions-handlers.js').findQ;
-var updateQ = require('./db/questions-handlers.js').updateQ;
-var removeQ = require('./db/questions-handlers.js').removeQ;
-var indexQ = require('./db/questions-handlers.js').indexQ;
+var insertQ = require('.././questions-handlers.js').insertQ;
+var findQ = require('.././questions-handlers.js').findQ;
+var updateQ = require('.././questions-handlers.js').updateQ;
+var removeQ = require('.././questions-handlers.js').removeQ;
+var indexQ = require('.././questions-handlers.js').indexQ;
 //Use proper url for MongoDB cluster
-var url = require('../../config.js').dbUrl;
+var url = require('../../../../config.js').dbUrl;
 
 
 app.use(cors());
@@ -22,7 +22,8 @@ app.use(bodyParser.json());
 
 //Fetches questions of a particular "questionType", selects one at random to send back
 //This should eventually be optimized to only send questions to which the user has not responded
-app.get('/questions', function(req, res, next) {
+app.get('/questions', function(req, res) {
+  //fetched questions are pushed to array
   let response = [];
 
   //Question genre id
@@ -49,7 +50,6 @@ app.get('/questions', function(req, res, next) {
 
 //Adds question to database
 app.post('/questions', function(req, res) {
-
   //The question object which will be added
   let question = req.body;
 
@@ -68,11 +68,29 @@ app.post('/questions', function(req, res) {
 
 //Delete a question from database
 app.post('/questions/remove', function(req, res) {
-  let id =
+  //id determines which question
+  let id = req.body.id;
+
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log('Connected to MongoDB server');
     removeQ(db, function() {
+      db.close();
+    });
+  });
+});
+
+//Updates certain parts of a question. Only admins should be able to this in response to bug reports
+app.post('/questions/update', function(req, res) {
+  //id determines which question
+  let id = req.body.id;
+  //property is an object where key:value pairs to be updated or added will be defined
+  let property = req.body.property
+
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log('Connected to MongoDB server');
+    updateQ(db, function() {
       db.close();
     });
   });
--- src/server/db/questions-handlers.js
+++ src/server/db/questions-handlers.js
@@ -2,25 +2,14 @@
 var MongoClient = require('mongodb').MongoClient
 , assert = require('assert');
 
-//Connection URL
-
-//This code is for UPDATE aka PUT
-// MongoClient.connect(url, function(err, db) {
-//   assert.equal(null, err);
-//   console.log('Connected to MongoDB server');
-//   updateDocument(db, function() {
-//     //closes connection with callback after inserting data
-//     db.close();
-//   });
-// });
 
 /********DATA METHODS********/
 
-//Using 'insertMany' to insert data to our collection
+//Using 'insert' to insert data to our collection
 var insertQ = function(db, callback) {
   //Specify the collection where we will 'insert' in this case 'questions'
   var collection = db.collection('questions');
-  //use insertMany to insert and array of JSON objects into collection
+  //can use 'insertMany' to insert and array of JSON objects into collection
   collection.insert(question, function(err, result) {
     assert.equal(err, null);
     assert.equal(1, result.result.n); //result.result Result sent from MongoDB
@@ -31,7 +20,7 @@ var insertQ = function(db, callback) {
   });
 }
 
-//Using 'find' to return data
+//Using 'find' to return data row(s)
 var findQ = function(db, callback) {
   //Specify the collection where we will 'find' in this case 'questions'
   var collection = db.collection('questions');
@@ -40,27 +29,26 @@ var findQ = function(db, callback) {
     .toArray(function(err, docs) {
       assert.equal(err, null);
       console.log('Found the following records...');
-      console.log(docs);
       callback(docs);
     });
 }
 
 //Update data row
 var updateQ = function(db, callback) {
-  //Specify the collection where we will 'find' in this case 'questions'
+  //Specify the collection where we will 'update' in this case 'questions'
   var collection = db.collection('questions');
   //Update selections where the first argument is the 'find' and the second argument is the 'replace'
-  collection.updateOne({_id: id}, { $set: {property}}, function(err, result) {
+  collection.updateOne({_id: id}, { $set: property}, function(err, result) {
     assert.equal(err, null);
     assert.equal(1, result.result.n);
-    console.log("Updated question where _id = ", id);
+    console.log("Updated question where _id = ", id, 'to contain', property);
     callback(result);
   })
 }
 
 //Remove data row
 var removeQ = function(db, callback) {
-  //Specify the collection where we will 'find' in this case 'questions'
+  //Specify the collection where we will 'remove' in this case 'questions'
   var collection = db.collection('questions');
   // Delete document where a is 3
   collection.deleteOne({ _id : id }, function(err, result) {
@@ -73,14 +61,13 @@ var removeQ = function(db, callback) {
 
 //Make a "hashed" index on collection
 var indexQ = function(db, callback) {
-  //Specify the collection where we will 'find' in this case 'questions'
+  //Specify the collection where we will 'index' in this case 'questions'
   db.collection('questions')
   .createIndex(
   {"prompt": 1},
   null,
   function(err, results) {
-    console.log("Created index on 'promt' property");
-    console.log(results);
+    console.log("Created index on 'prompt' property");
     callback();
   }
   )
