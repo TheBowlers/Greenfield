076b86af0aa8918126bfc8a3e5e38d5543a1171f
Zachary Biernat <zackbiernat@gmail.com>
2017-10-02 12:52:41 -0700

    Updated some variable names

--- src/server/router/server-config.js
+++ src/server/router/server-config.js
@@ -5,10 +5,10 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 
 //Question helper functions
-var handleGet = require('.././db/models/questions.js').getQuestion;
-var handlePost = require('.././db/models/questions.js').postQuestion;
-var handleDelete = require('.././db/models/questions.js').deleteQuestion;
-var handleUpdate = require('.././db/models/questions.js').updateQuestion;
+var handleGetQuestion = require('.././db/models/questions.js').getQuestion;
+var handlePostQuestion = require('.././db/models/questions.js').postQuestion;
+var handleDeleteQuestion = require('.././db/models/questions.js').deleteQuestion;
+var handleUpdateQuestion = require('.././db/models/questions.js').updateQuestion;
 
 
 
@@ -16,18 +16,20 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+/*QUESTION ROUTING*/
+
 //Fetches questions of a particular "questionType", selects one at random to send back
 //This should eventually be optimized to only send questions to which the user has not responded
-app.get('/questions', handleGet);
+app.get('/questions', handleGetQuestion);
 
 //Adds question to database, creates unizue '_id' which contains the timeStamp
-app.post('/questions', handlePost);
+app.post('/questions', handlePostQuestion);
 
 //Delete a question from database by '_id'
-app.post('/questions/remove', handleDelete);
+app.post('/questions/remove', handleDeleteQuestion);
 
 //Updates certain properties for a question bt '_id'
-app.post('/questions/update', handleUpdate);
+app.post('/questions/update', handleUpdateQuestion);
 
 
 module.exports = app;
