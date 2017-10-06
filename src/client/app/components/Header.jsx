c546655f433123a744cdbd8e45891b74627f1cad
Kent Shepard <kentshepard7@gmail.com>
2017-10-06 14:25:30 -0400

    Trigger modal leaderboard by clicking points

--- src/client/app/components/Header.jsx
+++ src/client/app/components/Header.jsx
@@ -33,7 +33,7 @@ class Header extends React.Component {
         <div className="nav navbar">
           <div className="container">
             <ul className="nav navbar-nav navbar-right">
-              <li><a href="#">{this.state.userScore} points</a><span></span></li>
+              <li><a href="#leaderboard" data-toggle="modal">{this.state.userScore} points</a><span></span></li>
               <li className="dropdown">
                 <a href="#" className="dropdown-toggle profile" data-toggle="dropdown" role="button"><img className="profile-pic" src={this.state.profilePic} /><span className="caret"></span></a>
                 <ul className="dropdown-menu" role="menu"  >
@@ -49,40 +49,38 @@ class Header extends React.Component {
             </ul>
           </div>
           <div className="bs-example">
-              <a href="#myModal" className="btn btn-lg btn-primary" data-toggle="modal">Launch Demo Modal</a>
-
-              <div id="myModal" className="modal fade">
-                  <div className="modal-dialog">
-                      <div className="modal-content">
-                          <div className="modal-header">
-                              <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                              <h4 className="modal-title">Leaderboard</h4>
-                          </div>
-                          <div className="modal-body">
-                            <table className="table table-striped table-hover ">
-                              <thead>
-                                <tr>
-                                  <th>User</th>
-                                  <th>Total <div>Score</div></th>
-                                  <th>Correct <div>Answers</div></th>
-                                  <th>Questions <div>Attempted</div></th>
-                                  <th>Percent</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr>
-                                  <td>Kent Shepard</td>
-                                  <td>100,000</td>
-                                  <td>75</td>
-                                  <td>150</td>
-                                  <td>50%</td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          </div>
-                      </div>
-                  </div>
-              </div>
+            <div id="leaderboard" className="modal fade">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 className="modal-title">Leaderboard</h4>
+                        </div>
+                        <div className="modal-body">
+                          <table className="table table-striped table-hover ">
+                            <thead>
+                              <tr>
+                                <th>User</th>
+                                <th>Total <div>Score</div></th>
+                                <th>Correct <div>Answers</div></th>
+                                <th>Questions <div>Attempted</div></th>
+                                <th>Percent</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>Kent Shepard</td>
+                                <td>100,000</td>
+                                <td>75</td>
+                                <td>150</td>
+                                <td>50%</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
           </div>
         </div>
 
